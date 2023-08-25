4be0ccbb0fa1df10fe4e342486f7f96903f3abed
Guilherme Goncalves <guilherme.g.braz@outlook.com>
2023-08-25 17:01:48 -0300

    -

--- src/XadrezPartida.hs
+++ src/XadrezPartida.hs
@@ -21,24 +21,3 @@ tabuleiroInicial = [
     where
         criaPecas :: Cor -> TipoPeca -> Peca
         criaPecas cor tipo = Peca tipo cor
-
--- tabuleiroInicial :: Tabuleiro
--- tabuleiroInicial = [
---     [ criaPecaPreto Torre 0 7, criaPecaPreto Cavalo 1 7, criaPecaPreto Bispo 2 7, criaPecaPreto Rainha 3 7
---     , criaPecaPreto Rei 4 7, criaPecaPreto Bispo 5 7, criaPecaPreto Cavalo 6 7, criaPecaPreto Torre 7 7
---     ],
---     [ criaPecaPreto Peao x 6 | x <- [0..7] ]
---     ] ++
---     [ replicate 8 (Empty (x, y)) | y <- [2..5], x <- [0..7] ] ++
---     [
---     [ criaPecaBranco Peao x 1 | x <- [0..7] ],
---     [ criaPecaBranco Torre 0 0, criaPecaBranco Cavalo 1 0, criaPecaBranco Bispo 2 0, criaPecaBranco Rainha 3 0
---     , criaPecaBranco Rei 4 0, criaPecaBranco Bispo 5 0, criaPecaBranco Cavalo 6 0, criaPecaBranco Torre 7 0
---     ]
---     ]
---     where
---         criaPecaPreto :: TipoPeca -> Int -> Int -> Casa
---         criaPecaPreto tipo x y = Ocupada (Peca tipo Preto) (x, y)
-        
---         criaPecaBranco :: TipoPeca -> Int -> Int -> Casa
---         criaPecaBranco tipo x y = Ocupada (Peca tipo Branco) (x, y)
